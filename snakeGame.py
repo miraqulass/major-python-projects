24deee41963ff226bbab370e56023241a591abfd
Oyogo Brian <92381805+miraqulass@users.noreply.github.com>
2023-11-10 17:17:41 +0300

    snake game update

--- snakeGame.py
+++ snakeGame.py
@@ -10,7 +10,7 @@ high_score = 0
 # Creating a window screen
 wn = turtle.Screen()
 wn.title("Snake Game")
-wn.bgcolor("blue")
+wn.bgcolor("black")
 # the width and height can be put as user's choice
 wn.setup(width=600, height=600)
 wn.tracer(0)
